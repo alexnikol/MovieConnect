8f9812dc9beff382a8c3965f9305fdab32c73e03
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-02-20 17:03:45 +0200

    Fix formatting of SceneDelegate and AppDelegate

--- MovieConnect/SceneDelegate.swift
+++ MovieConnect/SceneDelegate.swift
@@ -3,29 +3,28 @@
 import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = makeRootController()
         window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {}
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {}
-
+    
     func sceneWillResignActive(_ scene: UIScene) {}
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {}
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {}
     
     private func makeRootController() -> UIViewController {
         let vc = UIViewController()
-        vc.view.backgroundColor = .orange
         return vc
     }
 }
